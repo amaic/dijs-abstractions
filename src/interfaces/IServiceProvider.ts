10be8830186e000b957f605000ae269c1c172ae0
Alexander Mauri <maurialex@web.de>
2022-12-09 03:42:14 +0100

    extended interface with GetServices

--- src/interfaces/IServiceProvider.ts
+++ src/interfaces/IServiceProvider.ts
@@ -43,7 +43,43 @@ export default interface IServiceProvider
      * @param name optional name if named
      * @returns instance of service; if multiple registrations gets the latest registration
      */
-     GetRequiredService<INTERFACE>(serviceIdentifier: symbol, name?: string): INTERFACE;
+    GetRequiredService<INTERFACE>(serviceIdentifier: symbol, name?: string): INTERFACE;
+
+    /**
+     * Get desired services by unique service interface identifier. 
+     * @param serviceIdentifier unique service interface identifier
+     * @param name optional name if named
+     * @returns all instances of service or an empty array
+     */
+    GetServices(serviceIdentifier: symbol, name?: string): any[];
+
+    /**
+     * Generic variant.
+     * Get desired services by unique service interface identifier. 
+     * @param serviceIdentifier unique service interface identifier
+     * @param name optional name if named
+     * @returns all instances of service or an empty array
+     */
+    GetServices<INTERFACE>(serviceIdentifier: symbol, name?: string): INTERFACE[];
+
+    /**
+     * Get desired services by unique service interface identifier. 
+     * Throws exception if service does not exist.
+     * @param serviceIdentifier unique service interface identifier
+     * @param name optional name if named
+     * @returns all instances of service
+     */
+    GetRequiredServices(serviceIdentifier: symbol, name?: string): any[];
+
+    /**
+     * Generic variant.
+     * Get desired services by unique service interface identifier. 
+     * Throws exception if service does not exist.
+     * @param serviceIdentifier unique service interface identifier
+     * @param name optional name if named
+     * @returns all instances of service
+     */
+    GetRequiredServices<INTERFACE>(serviceIdentifier: symbol, name?: string): INTERFACE[];
 
     /** Create new service provider scope. */
     CreateScope(): IServiceProvider;
